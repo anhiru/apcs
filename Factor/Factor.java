ed03bcd51fd6d16d74b2ab7f146401c93b841364
andrew tran <31467090+wtrandrew@users.noreply.github.com>
2017-11-08 12:28:41 -0800

    Update Factor.java

--- Factor/Factor.java
+++ Factor/Factor.java
@@ -6,21 +6,18 @@ public class Factor {
         while(true) {
             System.out.print("Enter a positive integer: ");
             long value = Math.abs(in.nextLong());
-            long temp = value;
             System.out.print("Prime factors: ");
-            for(long i = 2; i <= temp; i++) {
-                while(temp % i == 0) {    // if temp is divisible by index i
+            for(long i = 2; i <= value; i++) {
+                while(value % i == 0) {    // if temp is divisible by index i
                     System.out.print(i + " ");
-                    temp /= i;
+                    value /= i;
                 }
             }
             System.out.println("");
             System.out.print("Continue? (Y/N) ");
             String answer = in.next().toLowerCase();
-            if(answer.equals("n") || answer.equals("no") || answer.equals("nah")) {
+            if(answer.startsWith("n")) {
                 break;
-            } else {
-                continue;
             }
         }
     } 
