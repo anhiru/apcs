8be004c9808da0c50c4e5dd3a3d17f5b2906e4eb
andrew tran <31467090+wtrandrew@users.noreply.github.com>
2017-11-08 17:28:11 -0800

    Update LeapYear.java

--- LeapYear/LeapYear.java
+++ LeapYear/LeapYear.java
@@ -10,36 +10,30 @@
 
 import java.util.Scanner; 
 
-public class LeapYear
-{
-    public static void main (String [] args)
-    {
-            boolean getOut = true;
-            
-            while(getOut == true) {
-                System.out.print("Enter a year: ");
-                Scanner input = new Scanner(System.in);
-            
-                int year = input.nextInt();
-                if(year > 1582) {
-                    if(year % 4 == 0 && year % 100 != 0) {
-                        System.out.println("It is a leap year."); 
-                    } else if(year % 400 == 0) {
-                        System.out.println("It is a leap year."); 
-                    } else {
-                        System.out.println("It is not a leap year."); 
-                    }
-                } else if(year % 4 == 0) {
+public class LeapYear {
+    public static void main(String[] args) {
+        while(true) {
+            System.out.print("Enter a year: ");
+            Scanner in = new Scanner(System.in);
+            int year = in.nextInt();
+            if(year > 1582) {
+                if(year % 4 == 0 && year % 100 != 0) {
+                    System.out.println("It is a leap year."); 
+                } else if(year % 400 == 0) {
                     System.out.println("It is a leap year."); 
                 } else {
                     System.out.println("It is not a leap year."); 
                 }
-                
-                System.out.print("Continue? [0 for no, any # for yes] " );     // zero stops the program
-                if(input.nextInt() == 0) {     // accepts only integer values
-                    getOut = false;
-                }
+            } else if(year % 4 == 0) {
+                System.out.println("It is a leap year."); 
+            } else {
+                System.out.println("It is not a leap year."); 
+            }    
+            System.out.print("Continue? [0 for no, any # for yes] " );    // zero stops the program
+            if(in.nextInt() == 0) {                                       // accepts only integer values
+                break;
             }
+        }
     }
 }
 
