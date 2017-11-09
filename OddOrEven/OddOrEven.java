5cf151a954909b22d6b4bffa948abd9db047d34e
andrew tran <31467090+wtrandrew@users.noreply.github.com>
2017-11-08 17:31:21 -0800

    Update OddOrEven.java

--- OddOrEven/OddOrEven.java
+++ OddOrEven/OddOrEven.java
@@ -1,25 +1,15 @@
-
 /**
  * OddOrEven PROGRAM
-   * An integer can be classified as either odd or even.
-   * This depends on whether or not the integer is completely divisible by 2.
-   * An even number is completely divisible by 2, and will have a remainder of 0.
-   * An odd number is not completely divisble by 2, and will have a remainder of 1/2.
  * @author (AndrewTran)
  * @version (08/21/2017)
  */
 
 import java.util.Scanner;
 
-public class OddOrEven
-{
-   public static void main(String[] args)
-   {
-       boolean getOut = true; 
-           
-       while(getOut == true) {
+public class OddOrEven {
+   public static void main(String[] args) {
+       while(true) {
            Scanner in = new Scanner(System.in);
-           
            System.out.print("Enter a number: ");
            long number = in.nextLong();
            if(number % 2 == 0) {
@@ -27,11 +17,10 @@ public class OddOrEven
            } else {
                System.out.println("The number is odd.");
            }
-           
-           System.out.print("Continue? (y/n) ");
-           String cont = in.next().toLowerCase();
-           if(cont.equals("n") || cont.equals("no") || cont.equals("nope") || cont.equals("nah")) {
-               getOut = false;
+           System.out.print("Continue? (Y/N) ");
+           String yn = in.nextLine().toLowerCase();
+           if(yn.startsWith("n") {
+               break;
            } 
        }
    }
