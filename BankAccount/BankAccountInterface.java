3365249d4dd8daae64886ffc28cbd01748ec565c
andrew tran <31467090+wtrandrew@users.noreply.github.com>
2017-11-08 17:21:22 -0800

    Update BankAccountInterface.java

--- BankAccount/BankAccountInterface.java
+++ BankAccount/BankAccountInterface.java
@@ -1,46 +1,46 @@
 import java.util.Scanner;
 
 public class BankAccountInterface {
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.print("What is your name? ");
-		String name = in.nextLine();
-		System.out.print("Please create a 4-digit pin number: ");
-		int pin = in.nextInt();
-		System.out.printf("Welcome to your new bank account, %s!", name);
-		BankAccount andrew = new BankAccount(pin);
-		double amount; int check; int newPin;
-		while(true) {
-			System.out.println();
-			System.out.println("Enter [1] to deposit money.");
-			System.out.println("Enter [2] to withdraw money.");
-			System.out.println("Enter [3] to view your current balance.");
-			System.out.println("Enter [4] to change your pin.");
-			int input = in.nextInt();
-			System.out.println();
-			if(input == 1) {
-				System.out.print("How much would you like to deposit? ");
-				amount = in.nextDouble();
-				andrew.deposit(amount); 
-			} else if(input == 2) {
-				System.out.print("How much would you like to withdraw? ");
-				amount = in.nextDouble();
-				andrew.withdraw(amount);
-			} else if(input == 3) {
-				System.out.println("$" + andrew.balance());
-			} else if(input == 4) {
-				System.out.print("Current pin: ");
-				check = in.nextInt();
-				if(andrew.matchPin(check)) {
-					System.out.print("New pin: ");
-					newPin = in.nextInt();
-					andrew.changePin(newPin);
-				} else {
-					System.out.println("Pin does not match.");
-				}
-			} else {
-				break;
-			}
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.print("What is your name? ");
+	String name = in.nextLine();
+	System.out.print("Please create a 4-digit pin number: ");
+	int pin = in.nextInt();
+	System.out.printf("Welcome to your new bank account, %s!", name);
+	BankAccount user = new BankAccount(pin);
+	double amount; int check; int newPin;
+	while(true) {
+	    System.out.println();
+	    System.out.println("Enter [1] to deposit money.");
+	    System.out.println("Enter [2] to withdraw money.");
+	    System.out.println("Enter [3] to view your current balance.");
+	    System.out.println("Enter [4] to change your pin.");
+	    int input = in.nextInt();
+	    System.out.println();
+	    if(input == 1) {
+		System.out.print("How much would you like to deposit? ");
+		amount = in.nextDouble();
+		user.deposit(amount); 
+	    } else if(input == 2) {
+		System.out.print("How much would you like to withdraw? ");
+		amount = in.nextDouble();
+		user.withdraw(amount);
+	    } else if(input == 3) {
+		System.out.println("$" + user.balance());
+	    } else if(input == 4) {
+		System.out.print("Current pin: ");
+		check = in.nextInt();
+		if(user.matchPin(check)) {
+		    System.out.print("New pin: ");
+		    newPin = in.nextInt();
+		    user.changePin(newPin);
+		} else {
+		    System.out.println("Pin does not match.");
 		}
+	    } else {
+		break;
+	    }
 	}
+    }
 }
