394dea46480fee0e1a711eba9afbc23ab6a2576e
Andrew Tran <31467090+wtrandrew@users.noreply.github.com>
2017-12-11 22:53:08 -0800

    Update WeeklyWageCalculator.java

--- WeeklyWageCalculator/WeeklyWageCalculator.java
+++ WeeklyWageCalculator/WeeklyWageCalculator.java
@@ -3,25 +3,12 @@ import java.util.Scanner;
 public class WeeklyWageCalculator {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int[] hours = new int[7];
-		System.out.print("How much do you get paid per day? $");
-		double wage = in.nextDouble();
-		System.out.println("Enter your schedule (in hours worked each day).");
-		System.out.print("Monday: ");
-		hours[1] = in.nextInt();
-		System.out.print("Tuesday: ");
-		hours[2] = in.nextInt();
-		System.out.print("Wednesday: ");
-		hours[3] = in.nextInt();
-		System.out.print("Thursday: ");
-		hours[4] = in.nextInt();
-		System.out.print("Friday: ");
-		hours[5] = in.nextInt();
-		System.out.print("Saturday: ");
-		hours[6] = in.nextInt();
-		System.out.print("Sunday: ");
-		hours[0] = in.nextInt();
-		WeeklyWage w = new WeeklyWage(hours, wage);
+
+		WeeklySchedule schedule = new WeeklySchedule();
+		schedule.setWage();
+		schedule.fillSchedule();
+		
+		WeeklyWage w = new WeeklyWage(schedule.getHours(), schedule.getWage());
 		boolean calc = true;
 
 		while(calc) {
@@ -31,30 +18,38 @@ public class WeeklyWageCalculator {
 			System.out.println("[3] Pay Cut");
 			System.out.println("[4] Total Hours");
 			System.out.println("[5] Overtime Hours");
-			System.out.println("[6] QUIT");
+			System.out.println("[6] Undertime Hours");
+			System.out.println("[7] View Schedule");
+			System.out.println("[8] QUIT");
 			int input = in.nextInt();
 			System.out.println();
 			switch(input) {
 				case 1:
-					System.out.println("Paycheck: $" + w.getWeeklyPay());
+					System.out.printf("Paycheck: $%.2f%n", w.getWeeklyPay());
 					break;
 				case 2:
 					System.out.print("Percent raise: ");
 					w.raise(in.nextDouble());
-					System.out.printf("New weekly wage: $%.2f%n", w.getWage());
+					System.out.printf("New wage: $%.2f%n", w.getWage());
 					break;
 				case 3:
 					System.out.print("Percent cut: ");
 					w.cut(in.nextDouble());
-					System.out.printf("New weekly wage: $%.2f%n", w.getWage());
+					System.out.printf("New wage: $%.2f%n", w.getWage());
 					break;
 				case 4:
-					System.out.println("Total number of hours worked this week: " + w.getTotalHours());
+					System.out.printf("Total number of hours worked this week: %d%n", w.getTotalHours());
 					break;
 				case 5:
-					System.out.println("Hours worked overtime this week: " + w.getTotalOvertimeHours());
+					System.out.printf("Hours worked overtime this week: %d%n", w.getTotalOvertimeHours());
 					break;
 				case 6:
+					System.out.printf("Hours worked undertime this week: %d%n", w.getTotalUndertimeHours());
+					break;
+				case 7:
+					schedule.viewSchedule();
+					break;
+				case 8:
 					calc = false;
 					break;
 				default:
