86bf00a52aaf3fa9ee08d396a3b27c01b3198ca4
Andrew Tran <31467090+wtrandrew@users.noreply.github.com>
2017-12-11 22:54:31 -0800

    Update WeeklyWage.java

--- WeeklyWageCalculator/WeeklyWage.java
+++ WeeklyWageCalculator/WeeklyWage.java
@@ -1,6 +1,7 @@
 public class WeeklyWage {
 	private int[] hours;
 	private double wage; 
+	
 	public WeeklyWage(int[] dailyHours, double hourlyWage) {
 		hours = dailyHours;
 		wage = hourlyWage;
@@ -24,25 +25,34 @@ public class WeeklyWage {
 	public int getTotalOvertimeHours() {
 		int overtime = 0;
 		for(int i = 0; i < hours.length; i++) {
-			if(i == 0 || i == hours.length-1) {
-				overtime += hours[i];
-			} else {
+			if(i < hours.length - 2) {
 				if(hours[i] > 8) {
-					overtime += hours[i]-8;
+					overtime += (hours[i] - 8);
 				}
+			} else {
+				overtime += hours[i];
 			}
 		}
 		return overtime;
 	}
+	public int getTotalUndertimeHours() {
+		int undertime = 0;
+		for(int i = 0; i < hours.length - 2; i++) {
+			if(hours[i] < 8) {
+				undertime += (8 - hours[i]);
+			}
+		}
+		return undertime;
+	}
 	public double getWeeklyPay() {
 		double pay = 0;
 		for(int i = 0; i < hours.length; i++) {
-			if(i == 0) {
+			if(i == hours.length - 2) {
 				pay += 2*wage*hours[i];
-			} else if(i == hours.length-1) {
+			} else if(i == hours.length - 1) {
 				pay += 1.5*wage*hours[i];
 			} else if(hours[i] > 8) {
-				pay += 1.5*wage*(hours[i]-8) + 8*wage;
+				pay += 1.5*wage*(hours[i] - 8) + 8*wage;
 			} else {
 				pay += wage*hours[i];
 			}
