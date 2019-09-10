2bd3eee80b2fecbacaf393fe714276c359234916
Andrew Tran <31467090+wtrandrew@users.noreply.github.com>
2019-09-10 09:32:09 -0700

    Update LeapYear.java

--- LeapYear/LeapYear.java
+++ LeapYear/LeapYear.java
@@ -16,19 +16,23 @@ public class LeapYear {
             System.out.print("Enter a year: ");
             Scanner in = new Scanner(System.in);
             int year = in.nextInt();
+            boolean leap_year = false;
+          
+            // before 1582 we only care about if the year is divisible by 4 
             if(year > 1582) {
-                if(year % 4 == 0 && year % 100 != 0) {
-                    System.out.println("It is a leap year."); 
-                } else if(year % 400 == 0) {
-                    System.out.println("It is a leap year."); 
-                } else {
-                    System.out.println("It is not a leap year."); 
+                if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                    leap_year = true;
                 }
             } else if(year % 4 == 0) {
-                System.out.println("It is a leap year."); 
+                leap_year = true;
+            }
+          
+            if(leap_year) {
+                System.out.println(year + " is a leap year.");
             } else {
-                System.out.println("It is not a leap year."); 
-            }    
+                System.out.println(year + " is not a leap year.");
+            }
+          
             System.out.print("Continue? [0 for no, any # for yes] " );    // zero stops the program
             if(in.nextInt() == 0) {                                       // accepts only integer values
                 break;
@@ -36,5 +40,3 @@ public class LeapYear {
         }
     }
 }
-
-// before 1582 we only care about if the year is divisible by 4 
